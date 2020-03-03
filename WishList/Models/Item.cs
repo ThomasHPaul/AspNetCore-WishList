e41c8b6ebca2b55b66eb82a5b2d135b67a87fb6f
ThomasHPaul <tpaul19@wgu.edu>
2020-03-03 07:44:07 -0600

    Added DbContext for InMemoryDatabase.

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "Please enter a description")]
+        [StringLength(50)]
+        public string Description { get; set; }
+
+    }
+}
